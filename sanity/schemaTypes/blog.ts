d9803d7efa6dd4dba618b15cd298622a1ceec899
hasiyapramodh <126224422+hasiyapramodh@users.noreply.github.com>
2024-03-13 13:43:47 +0530

    create blog.ts in sanity

--- sanity/schemaTypes/blog.ts
+++ sanity/schemaTypes/blog.ts
@@ -7,6 +7,36 @@ export default{
             name:'title',
             type:'string',
             title:'Title of a blog article'
+        },
+        {
+            name:'slug',
+            type:'slug',
+            title:'slug of your blog article',
+            options:{
+                source:'title'
+            },
+        },
+        {
+            name:'titleImage',
+            type:'image',
+            title:'Title image',
+
+        },
+        {
+            name:'smallDescription',
+            type:'text',
+            title:'small Description',
+        },
+        {
+            name:'content',
+            type:'array',
+            title:'Content',
+            of:[
+                {
+                    type:'block'
+                }
+            ]
+
         }
     ]
 }
