1957170d98507eec73f81bdee7eb8b193504ecb6
hasiyapramodh <126224422+hasiyapramodh@users.noreply.github.com>
2024-03-13 13:05:01 +0530

    add sanity

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,12 @@
+export default{
+    name:'blog',
+    type:'document',
+    title:'blog',
+    fields:[
+        {
+            name:'title',
+            type:'string',
+            title:'Title of a blog article'
+        }
+    ]
+}
