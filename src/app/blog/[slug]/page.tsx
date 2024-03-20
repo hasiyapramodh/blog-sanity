4173d7f421c14fff9081a4fe9c1536ac7dfb2aab
hasiyapramodh <126224422+hasiyapramodh@users.noreply.github.com>
2024-03-20 23:37:56 +0530

    u

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -32,19 +32,21 @@ export default async function BlogArticle({
                 <span className='block text-base text-center text-primary'></span>
                 <span className='mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl'>{data.title}</span>
             </h1>
+            <div className='m-auto'>
+                <Image
+                    src={urlFor(data.titleImage).url()}
+                    width={800}
+                    height={800}
+                    alt='Title Image'
+                    priority
+                    className='rounded-lg mt-8 border'
 
-            <Image
-                src={urlFor(data.titleImage).url()}
-                width={800}
-                height={800}
-                alt='Title Image'
-                priority
-                className='rounded-lg mt-8 border'
+                />
+            </div>
 
-            />
 
-            <div className='mt-16 prose prose-lg prose-li:marker:text-blue-500 prose-a:text-blue-700'>
-                <PortableText value={data.content}/>
+            <div className='m-auto  mt-16 prose prose-lg prose-li:marker:text-blue-500 prose-a:text-blue-700'>
+                <PortableText value={data.content} />
             </div>
 
 
