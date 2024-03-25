cdf23fd5ca64c8e80c68ebb01d5b48750c2c675e
hasiyapramodh <126224422+hasiyapramodh@users.noreply.github.com>
2024-03-25 14:30:37 +0530

    update contact.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,14 +6,13 @@ const ContactUs: React.FC = () => {
   return (
     <div className="container mx-auto px-4">
       {/* New image section with responsive behavior */}
-      <div className="image-container flex pl-10 pr-10">
-        <div className="w-1/3 max-md:hidden max-md:w-full">
+      <div className="image-container flex pl-10 pr-10 mt-[4rem]">
+        <div className="w-full  ">
           <Image
             src={Image1} // Replace with your actual image path
             alt="Contact Us"
-            width={400} // Set the image width (replace with actual width)
-            height={300} // Optional: Set height if needed
-            className="max-md:block max-md:w-full hover:rounded-full hover:brightness-150"
+           
+        className="max-md:block max-md:w-full hover:rounded-md hover:brightness-125 object-cover mb-9"
           />
         </div>
       </div>
@@ -21,11 +20,11 @@ const ContactUs: React.FC = () => {
       {/* Centered content section */}
       <div className="content flex flex-col items-center justify-content: center">
         {/* Contact Information section */}
-        <h1 className="text-3xl font-bold mb-8">Contact Us</h1>
-        <p className="mb-4">
+        <h1 className="text-3xl font-bold mb-8 font-head">Contact Us</h1>
+        <p className="mb-4 font-text">
           Get in touch with us if you have any questions, requests, or feedback. We'd love to hear from you!
         </p>
-        <ul className="list-disc pl-4">
+        <ul className="list-disc pl-4 font-text">
           {/* Replace placeholders with your actual contact information */}
           <li>Email: inyapa97@gmail.com</li>
           <li>Phone: (078) 587-9739</li>
